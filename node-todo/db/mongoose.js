b5abf22f6f437eacc650547954b8467adac643c1
David Trotz <davidtrotz@olivetree.com>
2018-09-28 13:24:18 -0700

    Work on tests

--- node-todo/db/mongoose.js
+++
@@ -1,6 +0,0 @@
-let mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/TodoApp', {useNewUrlParser: true});
-
-module.exports = {mongoose};
